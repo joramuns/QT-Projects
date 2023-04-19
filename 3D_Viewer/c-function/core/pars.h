ef4b4033a744c43f62ec1d37972d4a15c347777b
Mammie Mikken <mammiemi@mi-t4.msk.21-school.ru>
2023-04-19 22:18:35 +0300

    continue

--- 3D_Viewer/c-function/core/pars.h
+++ 3D_Viewer/c-function/core/pars.h
@@ -7,15 +7,17 @@
 #include <string.h>
 
 typedef struct pars_counters {
-    int count_vertex;
-    int count_side;
-    int size_sort_array;
-    int size_unsort_memory;
-    int size_sort_memory;
+  int count_vertex;
+  int count_side;
+  int size_sort_array;
+  int size_unsort_memory;
+  int size_sort_memory;
 } pars_counters;
 
 float *array_sort(char *filename, pars_counters *View);
 float my_atof(char *str);
 int my_atoi(const char *str);
 
+
 #endif // PARS_H
+
