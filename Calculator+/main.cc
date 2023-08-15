44574001a96b9f65f413791c0e25604cff9654da
Joramun Sasin HP <joramuns@student.21-school.ru>
2023-08-16 02:48:11 +0300

    chore(main): some primitive tests

--- Calculator+/main.cc
+++ Calculator+/main.cc
@@ -1,5 +1,16 @@
 #include "main.h"
+#include <iomanip>
 
 int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  double op = 91.9;
+  s21::Element test(op);
+  op = 13.888888;
+  s21::Element oist(op);
+  s21::Converter keka;
+  keka.AddOperand(test);
+  keka.AddOperand(oist);
+  std::cout << std::setprecision(15) << keka;
   return 0;
 }
