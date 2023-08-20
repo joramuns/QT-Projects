2c3569bfe18cc7493153336e3cb4905c32cdc56a
Joramun Sasin HP <joramuns@student.21-school.ru>
2023-08-20 19:30:16 +0300

    fix(postfix): handle multiple power operators, add comments

--- Calculator+/model/postfix.cc
+++ Calculator+/model/postfix.cc
@@ -13,24 +13,35 @@ void PostfixExpr::AddOperator(Element operation) noexcept {
     int operation_priority = operation.GetPriority();
     int operation_type = (int)operation.GetValue();
     int out_priority = queue_stack_.back().GetPriority();
+    /* Handle open bracket - push to queue */
     if (operation_type == s21::Element::kBracketOpen) {
       queue_stack_.push_back(operation);
+      /* Handle close bracket - pop out from queue until open bracket, then
+       * discard brackets */
     } else if (operation_type == s21::Element::kBracketClose) {
       FinalizeBrackets();
       if (!queue_stack_.empty()) {
         queue_stack_.pop_back();
-        if (!queue_stack_.empty() && queue_stack_.back().GetPriority() == 2) {
+        /* Pour unary operation */
+        if (!queue_stack_.empty() && out_priority == 2) {
           Pour();
         }
       }
-    } else if (operation_priority > out_priority) {
+      /* Handle greater priority operator - push it, or if there is a power
+       * operator in queue and current operator is power - do the same */
+    } else if (operation_priority > out_priority ||
+               (operation_priority == out_priority && out_priority == 3)) {
       queue_stack_.push_back(operation);
+      /* In case of equality of current and last queue operators - pop from
+       * queue and push last to output, push current to queue */
     } else if (operation_priority == out_priority) {
       stack_out_.push_back(queue_stack_.back());
       queue_stack_.pop_back();
       queue_stack_.push_back(operation);
+      /* Case when current operator priority is lesser than operator in queue.
+       * If there is power operation in queue - pop it, otherwise - pop all */
     } else {
-      if (!queue_stack_.empty() && queue_stack_.back().GetPriority() == 3) {
+      if (!queue_stack_.empty() && out_priority == 3) {
         Pour();
       } else {
         PourAll();
