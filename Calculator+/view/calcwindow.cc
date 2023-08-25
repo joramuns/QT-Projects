1976a3820e6a0436ed167c14ae6954d66abeb486
Joramun Sasin HP <joramuns@student.21-school.ru>
2023-08-25 23:58:52 +0300

    feat(view): all operators, evaluate buttons

--- Calculator+/view/calcwindow.cc
+++ Calculator+/view/calcwindow.cc
@@ -15,43 +15,129 @@ CalcWindow::CalcWindow(Model *cmodel) : QMainWindow(nullptr), model_(cmodel) {
   clear_->setGeometry(QRect(button_pos, q_button_size));
 
   /* Num Buttons */
-  for (int i = 9; i >= 0; --i) {
+  for (int i = 11; i >= 0; --i) {
     button_pos.setX(button_size * (i % 3) + offset_x);
     button_pos.setY(button_size * (i / 3) + offset_y);
 
-    QPushButton *new_button =
-        new QPushButton(QString::number(i < 9 ? (i + 1) : 0), this);
+    QPushButton *new_button;
+    if (i < 9) {
+      new_button = new QPushButton(QString::number(i + 1), this);
+    } else if (i == 9) {
+      new_button = new QPushButton("0", this);
+    } else if (i == 10) {
+      new_button = new QPushButton(".", this);
+    } else {
+      new_button = new QPushButton("e", this);
+    }
+
     new_button->setGeometry(QRect(button_pos, q_button_size));
-    /* connect(new_button, &QPushButton::released, &controller_, */
-    /*         [=]() { controller_.NumButton(i); }); */
     num_buttons_[i] = new_button;
   }
 
   /* Operators Buttons */
-  operator_buttons_[0] = new QPushButton("mod", this);
+  operator_buttons_[0] =
+      new OperatorButton(OperatorType::kModulus, "mod", this);
   button_pos.setX(150);
   button_pos.setY(0);
   operator_buttons_[0]->setGeometry(QRect(button_pos, q_button_size));
-  operator_buttons_[1] = new QPushButton("\u00F7", this);
+
+  operator_buttons_[1] =
+      new OperatorButton(OperatorType::kDivision, "\u00F7", this);
   button_pos.setX(150);
   button_pos.setY(50);
   operator_buttons_[1]->setGeometry(QRect(button_pos, q_button_size));
-  operator_buttons_[2] = new QPushButton("\u00D7", this);
+
+  operator_buttons_[2] =
+      new OperatorButton(OperatorType::kMultiplication, "\u00D7", this);
   button_pos.setX(150);
   button_pos.setY(100);
   operator_buttons_[2]->setGeometry(QRect(button_pos, q_button_size));
-  operator_buttons_[3] = new QPushButton("\u2212", this);
+
+  operator_buttons_[3] =
+      new OperatorButton(OperatorType::kSubtraction, "\u2212", this);
   button_pos.setX(150);
   button_pos.setY(150);
   operator_buttons_[3]->setGeometry(QRect(button_pos, q_button_size));
-  operator_buttons_[4] = new QPushButton("\u002B", this);
+
+  operator_buttons_[4] =
+      new OperatorButton(OperatorType::kAddition, "\u002B", this);
   button_pos.setX(150);
   button_pos.setY(200);
   operator_buttons_[4]->setGeometry(QRect(button_pos, q_button_size));
 
+  operator_buttons_[5] = new OperatorButton(OperatorType::kSin, "sin", this);
+  button_pos.setX(200);
+  button_pos.setY(0);
+  operator_buttons_[5]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[6] = new OperatorButton(OperatorType::kCos, "cos", this);
+  button_pos.setX(200);
+  button_pos.setY(50);
+  operator_buttons_[6]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[7] = new OperatorButton(OperatorType::kTan, "tan", this);
+  button_pos.setX(200);
+  button_pos.setY(100);
+  operator_buttons_[7]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[8] = new OperatorButton(OperatorType::kAsin, "asin", this);
+  button_pos.setX(250);
+  button_pos.setY(0);
+  operator_buttons_[8]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[9] = new OperatorButton(OperatorType::kAcos, "acos", this);
+  button_pos.setX(250);
+  button_pos.setY(50);
+  operator_buttons_[9]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[10] = new OperatorButton(OperatorType::kAtan, "atan", this);
+  button_pos.setX(250);
+  button_pos.setY(100);
+  operator_buttons_[10]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[11] =
+      new OperatorButton(OperatorType::kBracketOpen, "(", this);
+  button_pos.setX(50);
+  button_pos.setY(0);
+  operator_buttons_[11]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[12] =
+      new OperatorButton(OperatorType::kBracketClose, ")", this);
+  button_pos.setX(100);
+  button_pos.setY(0);
+  operator_buttons_[12]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[13] = new OperatorButton(OperatorType::kPower, "^", this);
+  button_pos.setX(200);
+  button_pos.setY(150);
+  operator_buttons_[13]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[14] = new OperatorButton(OperatorType::kSqrt, "sqrt", this);
+  button_pos.setX(200);
+  button_pos.setY(200);
+  operator_buttons_[14]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[15] = new OperatorButton(OperatorType::kLn, "ln", this);
+  button_pos.setX(300);
+  button_pos.setY(0);
+  operator_buttons_[15]->setGeometry(QRect(button_pos, q_button_size));
+
+  operator_buttons_[16] = new OperatorButton(OperatorType::kLog, "log", this);
+  button_pos.setX(300);
+  button_pos.setY(50);
+  operator_buttons_[16]->setGeometry(QRect(button_pos, q_button_size));
+
+  /* Evaluate button */
+  eval_ = new QPushButton("=", this);
+  button_pos.setX(250);
+  button_pos.setY(150);
+  q_button_size.setHeight(100);
+  eval_->setGeometry(QRect(button_pos, q_button_size));
+  eval_->setStyleSheet("background-color: #893101; font: white;");
+
   /* Display */
   display_ = new QLabel(this);
-  display_->setGeometry(QRect(QPoint(0, 250), QSize(150, 50)));
+  display_->setGeometry(QRect(QPoint(0, 250), QSize(350, 50)));
 }
 
 CalcWindow::~CalcWindow() {
--- Calculator+/view/calcwindow.h
+++ Calculator+/view/calcwindow.h
@@ -6,6 +6,7 @@
 #include <QPushButton>
 
 #include "../model/model.h"
+#include "operatorbutton.h"
 
 namespace s21 {
 class CalcWindow : public QMainWindow {
@@ -20,12 +21,34 @@ class CalcWindow : public QMainWindow {
   void ClearSlot();
 
  public:
-  std::array<QPushButton *, 10> num_buttons_;
-  std::array<QPushButton *, 5> operator_buttons_;
+  std::array<QPushButton *, 12> num_buttons_;
+  std::array<OperatorButton *, 17> operator_buttons_;
   QPushButton *clear_;
+  QPushButton *eval_;
   QLabel *display_;
   /* MVC */
   s21::Model *model_;
+
+ private:
+  enum OperatorType {
+    kAddition = 1,
+    kSubtraction,
+    kMultiplication = 10,
+    kDivision,
+    kModulus,
+    kPower,
+    kSin = 1000,
+    kCos,
+    kTan,
+    kAsin,
+    kAcos,
+    kAtan,
+    kSqrt,
+    kLn,
+    kLog,
+    kBracketOpen = 10000,
+    kBracketClose
+  };
 };
 }  // namespace s21
 #endif  // MAINWINDOW_H
