6ec9b612619a32685bb5bcb01eb675abf8fb29df
Joramun Sasin HP <joramuns@student.21-school.ru>
2023-08-25 21:20:15 +0300

    feat(controller): mvc decomposition, signals from numpad connected to slot

--- Calculator+/controller/controller.cc
+++ Calculator+/controller/controller.cc
@@ -1,7 +1,29 @@
 #include "controller.h"
 
+#include <QDebug>
+#include <QPushButton>
+
 namespace s21 {
-void Controller::ClearSlot2() { 
-  calcmodel_.ClearModel(); 
+Controller::Controller(CalcWindow *cview, Model *cmodel)
+    : view_(cview), model_(cmodel) {
+  for (double i = 0; i < 10; ++i)
+    connect(cview->num_buttons_[i], &QPushButton::released, this,
+            [=]() { NumButton(i); });
 }
+
+void Controller::NumButton(const double number) {
+  if (model_->LastIsOperator()) {
+    s21::Element e_number{number + 1};
+    model_->AddElement(e_number);
+  } else {
+    model_->AppendNumber(number + 1);
+  }
+  Render();
+}
+
+void Controller::Render() const noexcept {
+  std::string infix_line = model_->GetInfixString();
+  view_->display_->setText(QString::fromStdString(infix_line));
+}
+
 }  // namespace s21
--- Calculator+/controller/controller.h
+++ Calculator+/controller/controller.h
@@ -1,25 +1,32 @@
 #ifndef CONTROLLER_CONTROLLER_H_
 #define CONTROLLER_CONTROLLER_H_
 
+#include <QLabel>
 #include <QObject>
+
 #include "../model/model.h"
+#include "../view/calcwindow.h"
 
 namespace s21 {
 class Controller : public QObject {
   Q_OBJECT
 
  public:
-  Controller() = default;
+  Controller() = delete;
+  Controller(CalcWindow *cview, Model *cmodel);
   Controller(const Controller &other) = delete;
   Controller(Controller &&other) = delete;
   Controller &operator=(const Controller &other) = delete;
   Controller &operator=(Controller &&other) = delete;
 
- /* public slots: */
-  void ClearSlot2();
+ public slots:
+  void NumButton(const double i);
 
  private:
-  Model calcmodel_;
+  void Render() const noexcept;
+  /* MVC */
+  CalcWindow *view_;
+  Model *model_;
 };
 }  // namespace s21
 #endif  // CONTROLLER_CONTROLLER_H_
