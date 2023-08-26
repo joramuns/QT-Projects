07860309cdafb0b8351ccbbb641872545a18dccb
Joramun Sasin HP <joramuns@student.21-school.ru>
2023-08-27 00:42:44 +0300

    fix(element): set locale to parse with stod properly

--- Calculator+/main.cc
+++ Calculator+/main.cc
@@ -6,5 +6,7 @@ int main(int argc, char **argv) {
   s21::CalcWindow cview(&cmodel);
   s21::Controller controller(&cview, &cmodel);
   cview.show();
+  std::locale loc("en_US.utf8");
+  std::locale::global(loc);
   return app.exec();
 }
