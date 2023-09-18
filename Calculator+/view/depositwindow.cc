e93bc36945aa46d9c3e19a3ef1b8c1b1a16d30cf
joramuns <joramuns@student.21-school.ru>
2023-09-18 20:41:56 +0300

    feat(view): deposit window boundary values for spinboxes, replenishment and withdrawal lists, "add rep/wd" buttons connected

--- Calculator+/view/depositwindow.cc
+++ Calculator+/view/depositwindow.cc
@@ -3,7 +3,7 @@
 namespace s21 {
 DepositWindow::DepositWindow() {
   setAttribute(Qt::WA_DeleteOnClose, true);
-  setAttribute(Qt::WA_QuitOnClose, false );
+  setAttribute(Qt::WA_QuitOnClose, false);
   resize(400, 400);
   setWindowTitle("Deposit calculator");
 
@@ -11,31 +11,115 @@ DepositWindow::DepositWindow() {
 
   QGroupBox *input_group_box = AddInputGroupBox();
   QGroupBox *output_group_box = AddOutputGroupBox();
+  eval_deposit_ = new QPushButton("Deposit me!", this);
 
   main_layout->addWidget(input_group_box);
   main_layout->addWidget(output_group_box);
+  main_layout->addWidget(eval_deposit_);
   setLayout(main_layout);
 }
 
 DepositWindow::~DepositWindow(){};
 
+double DepositWindow::GetReplenishmentDay() {
+  return replenishment_day_->value();
+}
+
+double DepositWindow::GetReplenishmentAmount() {
+  return replenishment_amount_->value();
+}
+
+double DepositWindow::GetWithdrawalDay() {
+  return withdrawal_day_->value();
+}
+
+double DepositWindow::GetWithdrawalAmount() {
+  return withdrawal_amount_->value();
+}
+
+void DepositWindow::AddReplenishment(const QString &replenishment) {
+  replenishments_list_->addItem(replenishment);
+}
+
+void DepositWindow::ClearReplenishment() {
+  replenishments_list_->clear();
+}
+
+void DepositWindow::AddWithdrawal(const QString &withdrawal) {
+  withdrawals_list_->addItem(withdrawal);
+}
+
+void DepositWindow::ClearWithdrawal() {
+  withdrawals_list_->clear();
+}
+
 QGroupBox *DepositWindow::AddInputGroupBox() {
   QGroupBox *input_group_box = new QGroupBox(tr("Input data"));
 
+  deposit_amount_ = new QDoubleSpinBox;
+  deposit_amount_->setMinimum(0.01);
+  deposit_amount_->setMaximum(std::numeric_limits<double>::max());
+
+  deposit_term_ = new QDoubleSpinBox;
+  deposit_term_->setMinimum(1);
+  deposit_term_->setMaximum(std::numeric_limits<double>::max());
+  deposit_term_->setDecimals(0);
+
+  deposit_interest_ = new QDoubleSpinBox;
+  deposit_interest_->setMinimum(0);
+  deposit_interest_->setMaximum(std::numeric_limits<double>::max());
+
+  deposit_periodicity_ = new QComboBox;
+  deposit_periodicity_->addItem("monthly");
+  deposit_periodicity_->addItem("quarterly");
+  deposit_periodicity_->addItem("annualy");
+
+  capitalization_ = new QCheckBox;
+
+  replenishments_list_ = new QListWidget;
+
+  replenishment_day_ = new QDoubleSpinBox;
+  replenishment_day_->setDecimals(0);
+  replenishment_day_->setMinimum(1);
+  replenishment_day_->setMaximum(std::numeric_limits<double>::max());
+
+  replenishment_amount_ = new QDoubleSpinBox;
+  replenishment_amount_->setMinimum(0.01);
+  replenishment_amount_->setMaximum(std::numeric_limits<double>::max());
+
+  add_replenishment_ = new QPushButton("+");
+  remove_replenishment_ = new QPushButton("-");
+
+  withdrawals_list_ = new QListWidget;
+
+  withdrawal_day_ = new QDoubleSpinBox;
+  withdrawal_day_->setDecimals(0);
+  withdrawal_day_->setMinimum(1);
+  withdrawal_day_->setMaximum(std::numeric_limits<double>::max());
+
+  withdrawal_amount_ = new QDoubleSpinBox;
+  withdrawal_amount_->setMinimum(0.01);
+  withdrawal_amount_->setMaximum(std::numeric_limits<double>::max());
+
+  add_withdrawal_ = new QPushButton("+");
+  remove_withdrawal_ = new QPushButton("-");
+
   QFormLayout *layout = new QFormLayout;
-  layout->addRow(new QLabel(tr("Amount:")), new QDoubleSpinBox);
-  layout->addRow(new QLabel(tr("Term:")), new QDoubleSpinBox);
-  layout->addRow(new QLabel(tr("Interest rate:")), new QDoubleSpinBox);
-  layout->addRow(new QLabel(tr("Periodicity of payments:")), new QComboBox);
-  layout->addRow(new QLabel(tr("Capitalization of interest:")), new QCheckBox);
-  layout->addRow(new QLabel(tr("Replenishments:")), new QListWidget);
-  layout->addRow(new QLabel(tr("Day:")), new QDoubleSpinBox);
-  layout->addRow(new QLabel(tr("Amount:")), new QDoubleSpinBox);
-  layout->addRow(new QPushButton("+"), new QPushButton("-"));
-  layout->addRow(new QLabel(tr("Withdrawals:")), new QListWidget);
-  layout->addRow(new QLabel(tr("Day:")), new QDoubleSpinBox);
-  layout->addRow(new QLabel(tr("Amount:")), new QDoubleSpinBox);
-  layout->addRow(new QPushButton("+"), new QPushButton("-"));
+  layout->addRow(new QLabel(tr("Amount:")), deposit_amount_);
+  layout->addRow(new QLabel(tr("Term:")), deposit_term_);
+  layout->addRow(new QLabel(tr("Interest rate:")), deposit_interest_);
+  layout->addRow(new QLabel(tr("Periodicity of payments:")),
+                 deposit_periodicity_);
+  layout->addRow(new QLabel(tr("Capitalization of interest:")),
+                 capitalization_);
+  layout->addRow(new QLabel(tr("Replenishments:")), replenishments_list_);
+  layout->addRow(new QLabel(tr("Day:")), replenishment_day_);
+  layout->addRow(new QLabel(tr("Amount:")), replenishment_amount_);
+  layout->addRow(add_replenishment_, remove_replenishment_);
+  layout->addRow(new QLabel(tr("Withdrawals:")), withdrawals_list_);
+  layout->addRow(new QLabel(tr("Day:")), withdrawal_day_);
+  layout->addRow(new QLabel(tr("Amount:")), withdrawal_amount_);
+  layout->addRow(add_withdrawal_, remove_withdrawal_);
   input_group_box->setLayout(layout);
 
   return input_group_box;
--- Calculator+/view/depositwindow.h
+++ Calculator+/view/depositwindow.h
@@ -1,17 +1,17 @@
 #ifndef VIEW_DEPOSITWINDOW_H_
 #define VIEW_DEPOSITWINDOW_H_
 
+#include <QCheckBox>
+#include <QComboBox>
 #include <QDialog>
-#include <QWindow>
 #include <QDoubleSpinBox>
 #include <QFormLayout>
 #include <QGroupBox>
 #include <QLabel>
 #include <QLineEdit>
-#include <QComboBox>
-#include <QCheckBox>
 #include <QListWidget>
 #include <QPushButton>
+#include <QWindow>
 
 namespace s21 {
 class DepositWindow : public QWidget {
@@ -21,9 +21,46 @@ class DepositWindow : public QWidget {
   DepositWindow();
   ~DepositWindow();
 
+  /* Getters */
+  double GetReplenishmentDay();
+  double GetReplenishmentAmount();
+  double GetWithdrawalDay();
+  double GetWithdrawalAmount();
+
+  /* Mutators */
+  void AddReplenishment(const QString &replenishment);
+  void ClearReplenishment();
+  void AddWithdrawal(const QString &withdrawal);
+  void ClearWithdrawal();
+
+ public:
+  QPushButton *add_replenishment_;
+  QPushButton *remove_replenishment_;
+
+  QPushButton *add_withdrawal_;
+  QPushButton *remove_withdrawal_;
+
+  QPushButton *eval_deposit_;
+
  private:
   QGroupBox *AddInputGroupBox();
   QGroupBox *AddOutputGroupBox();
+
+ private:
+  /* Input block */
+  QDoubleSpinBox *deposit_amount_;
+  QDoubleSpinBox *deposit_term_;
+  QDoubleSpinBox *deposit_interest_;
+  QComboBox *deposit_periodicity_;
+  QCheckBox *capitalization_;
+
+  QListWidget *replenishments_list_;
+  QDoubleSpinBox *replenishment_day_;
+  QDoubleSpinBox *replenishment_amount_;
+
+  QListWidget *withdrawals_list_;
+  QDoubleSpinBox *withdrawal_day_;
+  QDoubleSpinBox *withdrawal_amount_;
 };
 }  // namespace s21
 #endif  // VIEW_DEPOSITWINDOW_H_
