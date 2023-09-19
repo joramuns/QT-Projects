7a000ef6d6c9fa61ce7868b86388495a66c562ba
joramuns <joramuns@student.21-school.ru>
2023-09-19 21:17:31 +0300

    feat(model): set data deposit, evaluate deposit

--- Calculator+/model/deposit.cc
+++ Calculator+/model/deposit.cc
@@ -27,6 +27,44 @@ void DepositCalc::RemoveWithdrawal(int number) noexcept {
   withdrawals_.erase(iter);
 }
 
+void DepositCalc::SetDepositData(double amount, int term,
+                                 double rate) noexcept {
+  amount_ = amount;
+  term_ = term;
+  interest_rate_ = rate / 100.0;
+}
+
+void DepositCalc::SetDepositType(int periodicity,
+                                 bool capitalization) noexcept {
+  deposit_type_ = periodicity;
+  capitalization_ = capitalization;
+}
+
+void DepositCalc::EvaluateDeposit() noexcept {
+  payoffs_.clear();
+  profit_amount_ = 0.0;
+  end_amount_ = 0.0;
+  tax_amount_ = 0.0;
+
+  int payoffs_number = term_ / deposit_type_;
+  if (payoffs_.capacity() < payoffs_number) {
+    payoffs_.reserve(payoffs_number);
+  }
+
+  double daily_profit = (amount_ * interest_rate_) / 365.0;
+  for (int day = 1; day <= term_; ++day) {
+    profit_amount_ += daily_profit;
+    if (day % deposit_type_ == 0 || day == term_) {
+      payoffs_.push_back(profit_amount_);
+      profit_amount_ = 0.0;
+      if (capitalization_) {
+        amount_ += payoffs_.back();
+        daily_profit = (amount_ * interest_rate_) / 365.0;
+      }
+    }
+  }
+}
+
 const std::map<double, double> &DepositCalc::GetReplenishmentList()
     const noexcept {
   return replenishments_;
@@ -35,6 +73,10 @@ const std::map<double, double> &DepositCalc::GetReplenishmentList()
 const std::map<double, double> &DepositCalc::GetWithdrawalList()
     const noexcept {
   return withdrawals_;
+}
+
+const std::vector<double> &DepositCalc::GetPayoffsList() const noexcept {
+  return payoffs_;
 }
 
 }  // namespace s21
--- Calculator+/model/deposit.h
+++ Calculator+/model/deposit.h
@@ -2,6 +2,7 @@
 #define MODEL_DEPOSIT_H_
 
 #include <map>
+#include <vector>
 
 namespace s21 {
 class DepositCalc final {
@@ -13,21 +14,30 @@ class DepositCalc final {
   void RemoveReplenishment(int number) noexcept;
   void AddWithdrawal(double day, double amount) noexcept;
   void RemoveWithdrawal(int number) noexcept;
+  void SetDepositData(double amount, int term, double rate) noexcept;
+  void SetDepositType(int periodicity, bool capitalization) noexcept;
+  void EvaluateDeposit() noexcept;
 
   /* Getters */
   const std::map<double, double> &GetReplenishmentList() const noexcept;
   const std::map<double, double> &GetWithdrawalList() const noexcept;
+  const std::vector<double> &GetPayoffsList() const noexcept;
 
  private:
  private:
   /* Input data */
   double amount_;
-  double term_;
+  int term_;
   double interest_rate_;
   int deposit_type_;
   bool capitalization_;
   std::map<double, double> replenishments_;
   std::map<double, double> withdrawals_;
+  /* Output data */
+  std::vector<double> payoffs_;
+  double profit_amount_;
+  double end_amount_;
+  double tax_amount_;
 };
 }  // namespace s21
 #endif  // MODEL_DEPOSIT_H_
