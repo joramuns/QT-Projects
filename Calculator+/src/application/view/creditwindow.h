986f8bfdbcb10030bae6fd54967d32132a671b55
joramuns <joramuns@student.21-school.ru>
2023-09-25 21:32:57 +0300

    chore(dvi): creditwindow documentation
    chore(dvi)

--- Calculator+/src/application/view/creditwindow.h
+++ Calculator+/src/application/view/creditwindow.h
@@ -1,3 +1,11 @@
+/**
+ * @file
+ * @brief Заголовочный файл с описанием класса CreditWindow
+ *
+ * Данный класс является дополнительным классом view приложения калькулятор.
+ * В нем содержатся поля ввода данных для расчета кредита, а также кнопка
+ * расчета кредита и поля вывода платежей и переплаты по кредиту.
+ */
 #ifndef VIEW_CREDITWINDOW_H_
 #define VIEW_CREDITWINDOW_H_
 
@@ -13,39 +21,110 @@
 #include <QWidget>
 
 namespace s21 {
+/**
+ * @brief Класс отображения дополнительного функционала приложения - кредитный
+ * калькулятор.
+ */
 class CreditWindow : public QWidget {
   Q_OBJECT
 
  public:
+  /**
+   * @brief Конструктор класса по умолчанию, инициализирует приватные поля
+   */
   CreditWindow();
 
   /* Accessors */
+  /**
+   * @brief Функция, возвращающая значение поля ввода суммы кредита
+   *
+   * @return Сумма кредита
+   */
   const double GetCreditAmount() const noexcept;
+  /**
+   * @brief Функция, возвращающая значение поля ввода срока кредита
+   *
+   * @return Срок кредита
+   */
   const double GetCreditTerm() const noexcept;
+  /**
+   * @brief Функция, возвращающая значение поля ввода процентной ставки кредита
+   *
+   * @return Процентная ставка
+   */
   const double GetCreditRate() const noexcept;
+  /**
+   * @brief Функция, возвращающая тип кредита
+   *
+   * @return 1 - дифференцированный, 0 - аннуитетный
+   */
   const int GetCreditType() const noexcept;
 
   /* Mutators */
+  /**
+   * @brief Функция очистки поля вывода платежей
+   */
   void ClearPayments() noexcept;
+  /**
+   * @brief Функция установки значений полей вывода
+   *
+   * @param over_payment Переплата по кредиту
+   * @param total_payment Общий платеж по кредиту
+   * @param monthly_payments Ежемесячные платежи
+   */
   void OutputData(double over_payment, double total_payment,
                   std::vector<double> monthly_payments);
 
  public:
+  /**
+   * @brief Указатель на кнопку вычисления кредитных условий
+   */
   QPushButton *eval_credit_;
 
  private:
+  /**
+   * @brief Функция инициализации полей ввода
+   *
+   * @return Указатель на виджет с полями ввода
+   */
   QGroupBox *AddInputGroupBox();
+  /**
+   * @brief Функция инициализации полей вывода
+   *
+   * @return Указатель на виджет с полями вывода
+   */
   QGroupBox *AddOutputGroupBox();
 
  private:
   /* Input block */
+  /**
+   * @brief Указатель на поле ввода размера кредита
+   */
   QDoubleSpinBox *credit_amount_;
+  /**
+   * @brief Указатель на поле ввода срока кредита
+   */
   QDoubleSpinBox *credit_term_;
+  /**
+   * @brief Указатель на поле ввода процентной ставки по кредиту
+   */
   QDoubleSpinBox *credit_interest_;
+  /**
+   * @brief Указатель на поле ввода типа кредита
+   */
   QComboBox *credit_type_;
   /* Output block */
+  /**
+   * @brief Указатель на лист полей вывода платежей по кредиту
+   */
   QListWidget *payment_monthly_;
+  /**
+   * @brief Указатель на поле вывода переплаты по кредиту
+   */
   QLineEdit *payment_over_;
+  /**
+   * @brief Указатель на поле вывода общей суммы платежа по кредиту
+   */
   QLineEdit *payment_total_;
 };
 
