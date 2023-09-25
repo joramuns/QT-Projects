c3a8cd2389cdb86f6291e3a35031713fbed8a49a
joramuns <joramuns@student.21-school.ru>
2023-09-25 20:17:27 +0300

    fix(model): refactor infix model, move to private section functions lastisoperator and lastisvariable

--- Calculator+/src/application/model/infix.cc
+++ Calculator+/src/application/model/infix.cc
@@ -15,13 +15,6 @@ const std::string InfixExpr::GetInfixString() const noexcept {
   return result;
 }
 
-bool InfixExpr::LastIsOperator() const noexcept {
-  return infix_data_.empty() ? true : infix_data_.back()->IsOperator();
-}
-
-bool InfixExpr::LastIsVariable() const noexcept {
-  return infix_data_.empty() ? false : infix_data_.back()->IsVariable();
-}
 void InfixExpr::AddElement(const int type) {
   auto e_operator = std::make_unique<Element>(type);
   AddElement(std::move(e_operator));
@@ -82,6 +75,14 @@ void InfixExpr::SetVariables(const double number) noexcept {
 /* void InfixExpr::DelElement() noexcept { infix_data_.pop_back(); } */
 
 /* Private infix functions */
+
+bool InfixExpr::LastIsOperator() const noexcept {
+  return infix_data_.empty() ? true : infix_data_.back()->IsOperator();
+}
+
+bool InfixExpr::LastIsVariable() const noexcept {
+  return infix_data_.empty() ? false : infix_data_.back()->IsVariable();
+}
 
 int InfixExpr::SizeValid(const elem_iterator iter_begin) const noexcept {
   int ex_code = 0;
