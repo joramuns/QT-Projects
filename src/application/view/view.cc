d88d15eca10957a0dd9fc946c4b7a9390e943f58
joramuns <joramuns@student.21-school.ru>
2023-11-21 21:17:49 +0300

    chore(view): view class init

---
+++ src/application/view/view.cc
@@ -0,0 +1,5 @@
+#include "view.h"
+
+namespace s21 {
+View::View() : QMainWindow(nullptr) { setFixedSize(500, 500); }
+}  // namespace s21
---
+++ src/application/view/view.h
@@ -0,0 +1,18 @@
+#ifndef VIEW_H_
+#define VIEW_H_
+
+#include <QMainWindow>
+#include <QWidget>
+
+namespace s21 {
+class View final : public QMainWindow {
+  Q_OBJECT
+
+ public:
+    View();
+
+ private:
+};
+}  // namespace s21
+
+#endif  // VIEW_H_
