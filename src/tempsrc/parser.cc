d00aac5878ab7282fafbb79b87084921abb2da5d
Mammie Mikken <mammiemi@ga-b3.msk.21-school.ru>
2023-11-21 22:02:52 +0300

    feat: add pars normals, textures

--- src/tempsrc/parser.cc
+++ src/tempsrc/parser.cc
@@ -6,46 +6,50 @@ Parser::Parser(const std::string &filename) { ReadObj(filename); }
 /* Private functions */
 int Parser::ReadObj(const std::string &filename) {
   std::ifstream file;
-  try {
-    file.open(filename, std::ios::in);
-    /* file.exceptions(std::ifstream::failbit); */
-    std::cout << "here" << std::endl;
-    std::string line;
-    if (file) {
-    } else {
-      std::cout << "opana" << std::endl;
-    }
+  file.open(filename, std::ios::in);
+  std::string line;
+  if (!file) {
+    return BAD_FILENAME;
+  } else {
     while (std::getline(file, line)) {
       std::string prefix = line.substr(0, 2);
+      std::istringstream data(line.substr(2));
+      std::cout << line;
       if (prefix == "v ") {
-        std::istringstream data(line.substr(2));
-        point vertex;
-        data >> vertex.x_;
-        data >> vertex.y_;
-        data >> vertex.z_;
-        data >> vertex.a_;
-        std::cout << vertex.x_ << " " << vertex.y_ << " " << vertex.z_ << " "
-                  << vertex.a_ << std::endl;
+        PointCoordinates vertex;
+        data >> vertex.x;
+        data >> vertex.y;
+        data >> vertex.z;
+        data >> vertex.a;
+        vertices_.push_back(vertex);
+      } else if (prefix == "vt") {
+        TexturesCoordinates textures;
+        data >> textures.u;
+        data >> textures.v;
+        data >> textures.w;
+        texture_points_.push_back(textures);
+      } else if (prefix == "vn") {
+        NormalsCoordinate normals;
+        data >> normals.x;
+        data >> normals.y;
+        data >> normals.z;
+        normals_points_.push_back(normals);
       } else if (prefix == "f ") {
-        std::istringstream data(line.substr(2));
-        std::string group;
-        while (data >> group) {
-          /* std::vector<f_element> element; */
-          std::istringstream data_group(group);
-          f_element e_temp;
-          char temp;
-          /* int i_temp; */
-          data_group >> e_temp.v_ >> temp >> e_temp.vt_ >> temp >> e_temp.vn_;
-          std::cout << "origin " << group << std::endl;
-          std::cout << "ho ho " << e_temp.v_ << " " << e_temp.vt_ << " " << e_temp.vn_ << std::endl;
-
+        // std::istringstream data(line.substr(2));
+        // std::string group;
+        // while (data >> group) {
+        //   /* std::vector<f_element> element; */
+        //   std::istringstream data_group(group);
+        //   char temp;
+        //   /* int i_temp; */
+        //   data_group >> e_temp.v_ >> temp >> e_temp.vt_ >> temp >> e_temp.vn_;
+        //   std::cout << "origin " << group << std::endl;
+        //   std::cout << "ho ho " << e_temp.v_ << " " << e_temp.vt_ << " "
+        //             << e_temp.vn_ << std::endl;
         }
       }
     }
-  } catch (const std::ifstream::failure &e) {
-    std::cout << "Wrong filename" << std::endl;
-  }
   return 0;
-}
+  }
 
-}  // namespace s21
+} // namespace s21
