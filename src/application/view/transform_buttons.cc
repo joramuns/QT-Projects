5f45af20cd69bd50c0990f1bf7c69e5217e87336
joramuns <joramuns@student.21-school.ru>
2023-11-22 19:47:31 +0300

    feat(view): transformation buttons tab class, button class, size and order definitions

--- src/application/view/transform_buttons.cc
+++ src/application/view/transform_buttons.cc
@@ -1,46 +1,22 @@
 #include "transform_buttons.h"
 
-#define X_INCREASE_ROW 1
-#define X_INCREASE_COL 2
-#define Y_INCREASE_ROW 0
-#define Y_INCREASE_COL 1
-#define Z_INCREASE_ROW 0
-#define Z_INCREASE_COL 2
-#define X_DECREASE_ROW 1
-#define X_DECREASE_COL 0
-#define Y_DECREASE_ROW 1
-#define Y_DECREASE_COL 1
-#define Z_DECREASE_ROW 0
-#define Z_DECREASE_COL 0
-
-#define L_STEP_ROW 2
-#define L_STEP_COL 0
-#define STEP_ROW 2
-#define STEP_COL 1
-
-#define S_INCREASE_ROW 0
-#define S_INCREASE_COL 0
-#define S_DECREASE_ROW 1
-#define S_DECREASE_COL 0
-
-#define S_L_STEP_ROW 0
-#define S_L_STEP_COL 1
-#define S_STEP_ROW 1
-#define S_STEP_COL 1
-
 namespace s21 {
-TransformButtons::TransformButtons(const QChar labels[2][3]) : QWidget(nullptr) {
+TransformButtons::TransformButtons(const QChar labels[2][3])
+    : QWidget(nullptr) {
+  setFixedSize(3 * BUTTON_SIZE + 2 * SPACING,
+               2 * SPACING + 2 * BUTTON_SIZE + BUTTON_SIZE / 2);
   QGridLayout *layout = new QGridLayout;
+  layout->setSpacing(SPACING);
   setLayout(layout);
 
-  x_increase_ = new QPushButton('X' + labels[X_INCREASE_ROW][X_INCREASE_COL]);
-  x_decrease_ = new QPushButton(labels[X_DECREASE_ROW][X_DECREASE_COL] + 'X');
+  x_increase_ = new TButton('X' + labels[X_INCREASE_ROW][X_INCREASE_COL]);
+  x_decrease_ = new TButton(labels[X_DECREASE_ROW][X_DECREASE_COL] + 'X');
 
-  y_increase_ = new QPushButton('Y' + labels[Y_INCREASE_ROW][Y_INCREASE_COL]);
-  y_decrease_ = new QPushButton('Y' + labels[Y_DECREASE_ROW][Y_DECREASE_COL]);
+  y_increase_ = new TButton('Y' + labels[Y_INCREASE_ROW][Y_INCREASE_COL]);
+  y_decrease_ = new TButton('Y' + labels[Y_DECREASE_ROW][Y_DECREASE_COL]);
 
-  z_increase_ = new QPushButton('Z' + labels[Z_INCREASE_ROW][Z_INCREASE_COL]);
-  z_decrease_ = new QPushButton(labels[Z_DECREASE_ROW][Z_DECREASE_COL] + 'Z');
+  z_increase_ = new TButton('Z' + labels[Z_INCREASE_ROW][Z_INCREASE_COL]);
+  z_decrease_ = new TButton(labels[Z_DECREASE_ROW][Z_DECREASE_COL] + 'Z');
 
   step_ = new QDoubleSpinBox;
 
@@ -55,11 +31,14 @@ TransformButtons::TransformButtons(const QChar labels[2][3]) : QWidget(nullptr) 
 }
 
 ScaleButtons::ScaleButtons() : QWidget(nullptr) {
+  setFixedSize(2 * BUTTON_SIZE +  SPACING,
+               SPACING + BUTTON_SIZE + BUTTON_SIZE / 2);
   QGridLayout *layout = new QGridLayout;
+  layout->setSpacing(SPACING);
   setLayout(layout);
 
-  increase_ = new QPushButton("+");
-  decrease_ = new QPushButton("-");
+  increase_ = new TButton("+");
+  decrease_ = new TButton("-");
 
   step_ = new QDoubleSpinBox;
 
@@ -67,5 +46,29 @@ ScaleButtons::ScaleButtons() : QWidget(nullptr) {
   layout->addWidget(decrease_, S_DECREASE_ROW, S_DECREASE_COL);
   layout->addWidget(new QLabel("Step: "), S_L_STEP_ROW, S_L_STEP_COL);
   layout->addWidget(step_, S_STEP_ROW, S_STEP_COL);
+}
+
+TButton::TButton(const QString &label) : QPushButton(label) {
+  setFixedSize(BUTTON_SIZE, BUTTON_SIZE);
+  /* setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum); */
+  setStyleSheet("background-color: #893101; font: white;");
+}
+
+TransformationTab::TransformationTab() {
+  const QChar rotate_labels[2][3] = {
+      {QChar(0x000027F2), QChar(0x000027F3), QChar(0x000027F3)},
+      {QChar(0x000027F2), QChar(0x000027F2), QChar(0x000027F3)}};
+  rotate_buttons_ = new TransformButtons(rotate_labels);
+
+  const QChar move_labels[2][3] = {
+      {QChar(0x00002357), QChar(0x00002191), QChar(0x00002350)},
+      {QChar(0x00002190), QChar(0x00002193), QChar(0x00002192)}};
+  move_buttons_ = new TransformButtons(move_labels);
+
+  scale_buttons_ = new ScaleButtons();
+  
+  addTab(rotate_buttons_, "Rotation");
+  addTab(move_buttons_, "Movement");
+  addTab(scale_buttons_, "Scaling");
 }
 }  // namespace s21
--- src/application/view/transform_buttons.h
+++ src/application/view/transform_buttons.h
@@ -3,11 +3,44 @@
 
 #include <QDoubleSpinBox>
 #include <QGridLayout>
-#include <QPushButton>
-#include <QWidget>
+#include <QGroupBox>
 #include <QLabel>
+#include <QPushButton>
+#include <QTabWidget>
+#include <QWidget>
 #include <string>
 #include <vector>
+
+#define X_INCREASE_ROW 1
+#define X_INCREASE_COL 2
+#define Y_INCREASE_ROW 0
+#define Y_INCREASE_COL 1
+#define Z_INCREASE_ROW 0
+#define Z_INCREASE_COL 2
+#define X_DECREASE_ROW 1
+#define X_DECREASE_COL 0
+#define Y_DECREASE_ROW 1
+#define Y_DECREASE_COL 1
+#define Z_DECREASE_ROW 0
+#define Z_DECREASE_COL 0
+
+#define L_STEP_ROW 2
+#define L_STEP_COL 0
+#define STEP_ROW 2
+#define STEP_COL 1
+
+#define S_INCREASE_ROW 0
+#define S_INCREASE_COL 0
+#define S_DECREASE_ROW 0
+#define S_DECREASE_COL 1
+
+#define S_L_STEP_ROW 1
+#define S_L_STEP_COL 0
+#define S_STEP_ROW 1
+#define S_STEP_COL 1
+
+#define BUTTON_SIZE 80
+#define SPACING 30
 
 namespace s21 {
 class TransformButtons final : public QWidget {
@@ -41,6 +74,24 @@ class ScaleButtons final : public QWidget {
   QPushButton *decrease_;
 
   QDoubleSpinBox *step_;
+};
+
+class TButton final : public QPushButton {
+  Q_OBJECT
+ public:
+  explicit TButton(const QString &text);
+};
+
+class TransformationTab final : public QTabWidget {
+  Q_OBJECT
+
+  public:
+    TransformationTab();
+
+  private:
+    TransformButtons *rotate_buttons_;
+    TransformButtons *move_buttons_;
+    ScaleButtons *scale_buttons_;
 };
 }  // namespace s21
 
