c351b1cb96ed6d82c022dd9f384b75221851254d
Mammie Mikken <mammiemi@ga-b3.msk.21-school.ru>
2023-11-22 22:55:48 +0300

    chage test case

--- src/tempsrc/test_parser.cc
+++ src/tempsrc/test_parser.cc
@@ -6,7 +6,7 @@ void wrong_filename() {
 }
 
 void true_filename() {
-  s21::Parser test("../../data-samples/just.obj");
+  s21::Parser test("../../data-samples/test_two.obj");
   std::cout << "Success" << std::endl;
 }
 
