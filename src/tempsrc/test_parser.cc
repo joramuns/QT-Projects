866c0e9c660ef35bf86f068560eabb3c996ae24f
Mammie Mikken <mammiemi@ga-b3.msk.21-school.ru>
2023-11-27 16:03:07 +0300

    conflict accepted

--- src/tempsrc/test_parser.cc
+++ src/tempsrc/test_parser.cc
@@ -6,7 +6,7 @@ void wrong_filename() {
 }
 
 void true_filename() {
-  s21::Parser test("../../data-samples/test_two.obj");
+  s21::Parser test("../../data-samples/test_one.obj");
   std::cout << "Success" << std::endl;
 }
 
