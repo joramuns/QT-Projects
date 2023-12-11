4ce70b211bf0024843be20dbdef5595f0330067b
joramuns <joramuns@student.21-school.ru>
2023-12-11 22:51:01 +0300

    feat(view): transform class

---
+++ src/application/view/opengl/transform_class.cc
@@ -0,0 +1,25 @@
+#include "transform_class.h"
+
+// TEMP
+#include <iostream>
+
+namespace s21 {
+  Axes::Axes(double init) : x_(init), y_(init), z_(init) {}
+
+  void Axes::Change(double value, char axis) {
+  std::cout << "Rotate glwidget " << x_ << " " << y_ << " " << z_ << " " << axis << std::endl;
+    if (axis == 'X') {
+      x_ += static_cast<GLfloat>(value);
+    } else if (axis == 'Y') {
+      y_ += static_cast<GLfloat>(value);
+    } else if (axis == 'Z') {
+      z_ += static_cast<GLfloat>(value);
+    } else if (axis == 'A') {
+      x_ = y_ = z_ += value;
+    }
+  }
+
+  QVector3D Axes::GetChangeVector() {
+    return QVector3D{x_, y_, z_};
+  }
+}
---
+++ src/application/view/opengl/transform_class.h
@@ -0,0 +1,20 @@
+#ifndef VIEW_OPENGL_TRANSFORM_CLASS_H_
+#define VIEW_OPENGL_TRANSFORM_CLASS_H_
+
+#include <QOpenGLShaderProgram>
+
+namespace s21 {
+class Axes {
+ public:
+  Axes() = default;
+  explicit Axes(double init);
+  void Change(double value, char axis);
+  QVector3D GetChangeVector();
+
+ private:
+  GLfloat x_;
+  GLfloat y_;
+  GLfloat z_;
+};
+}  // namespace s21
+#endif  // VIEW_OPENGL_TRANSFORM_CLASS_H_
