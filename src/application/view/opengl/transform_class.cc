5ac58ce37fc3d15eb680de86f5ccd3e664c329b2
Mammie Mikken <mammiemi@ga-b3.msk.21-school.ru>
2024-01-14 15:12:50 +0300

    fix(view): style

--- src/application/view/opengl/transform_class.cc
+++ src/application/view/opengl/transform_class.cc
@@ -4,22 +4,21 @@
 #include <iostream>
 
 namespace s21 {
-  Axes::Axes(double init) : x_(init), y_(init), z_(init) {}
+Axes::Axes(double init) : x_(init), y_(init), z_(init) {}
 
-  void Axes::Change(double value, char axis) {
-  std::cout << "Rotate glwidget " << x_ << " " << y_ << " " << z_ << " " << axis << std::endl;
-    if (axis == 'X') {
-      x_ += static_cast<GLfloat>(value);
-    } else if (axis == 'Y') {
-      y_ += static_cast<GLfloat>(value);
-    } else if (axis == 'Z') {
-      z_ += static_cast<GLfloat>(value);
-    } else if (axis == 'A') {
-      x_ = y_ = z_ += value;
-    }
-  }
-
-  QVector3D Axes::GetChangeVector() {
-    return QVector3D{x_, y_, z_};
+void Axes::Change(double value, char axis) {
+  std::cout << "Rotate glwidget " << x_ << " " << y_ << " " << z_ << " " << axis
+            << std::endl;
+  if (axis == 'X') {
+    x_ += static_cast<GLfloat>(value);
+  } else if (axis == 'Y') {
+    y_ += static_cast<GLfloat>(value);
+  } else if (axis == 'Z') {
+    z_ += static_cast<GLfloat>(value);
+  } else if (axis == 'A') {
+    x_ = y_ = z_ += value;
   }
 }
+
+QVector3D Axes::GetChangeVector() { return QVector3D{x_, y_, z_}; }
+} // namespace s21
