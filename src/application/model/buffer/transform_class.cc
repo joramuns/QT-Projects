0eb83262d46200d5dff2cb871e3a7e3db5acd468
joramuns <joramuns@student.21-school.ru>
2024-02-08 23:07:12 +0300

    fix(model): scaling issue below zero

--- src/application/model/buffer/transform_class.cc
+++ src/application/model/buffer/transform_class.cc
@@ -7,8 +7,6 @@ namespace s21 {
 Axes::Axes(double init) : x_(init), y_(init), z_(init) {}
 
 void Axes::Change(double value, char axis) {
-  std::cout << "Rotate glwidget " << x_ << " " << y_ << " " << z_ << " " << axis
-            << std::endl;
   if (axis == 'X') {
     x_ += static_cast<GLfloat>(value);
   } else if (axis == 'Y') {
@@ -16,9 +14,11 @@ void Axes::Change(double value, char axis) {
   } else if (axis == 'Z') {
     z_ += static_cast<GLfloat>(value);
   } else if (axis == 'A') {
-    x_ = y_ = z_ += value;
+    if (-value <= x_) {
+      x_ = y_ = z_ += value;
+    }
   }
 }
 
 QVector3D Axes::GetChangeVector() { return QVector3D{x_, y_, z_}; }
-} // namespace s21
+}  // namespace s21
