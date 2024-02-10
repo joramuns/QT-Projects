6519774678a5e13e1b29c7e8f380203602401a07
mammiemi <mammiemi@student.21-school.ru>
2024-02-10 15:01:09 +0300

    docs: facade documentation

--- src/application/model/facade/facade.h
+++ src/application/model/facade/facade.h
@@ -1,3 +1,10 @@
+/*!
+\file
+\brief Заголовочный файл с описанием класса.
+
+Данный файл содержит в себе определения класса фасада, дающего пользователю
+интерфейс взаимодействия с моделью, скрывая внутреннюю реализацию.
+*/
 #ifndef CPP4_3DVIEWER_V2_MODEL_FACADE_FACADE_H
 #define CPP4_3DVIEWER_V2_MODEL_FACADE_FACADE_H
 
@@ -8,39 +15,99 @@
 #include "../parser/parser.h"
 
 namespace s21 {
+/// @brief Класс реализует паттерн Фасад, дающий пользователю интерфейс
+/// взаимодействия с моделью 3D_Viewer
 class Facade {
- public:
+public:
+  /// @brief Конструктор по умолчанию
   Facade();
+
+  /// @brief Деструктор класса
   ~Facade();
 
+  /// @brief Метод добавляет в модель обработку нового файла 3-х мерного объекта
+  /// @param filename Абсолютный путь к файлу 3-х мерной модели в формате obj
   void AddModel(const std::string &filename) noexcept;
+
+  /// @brief Метод удаляет из модели данные о 3-х мерном объекте
+  /// @param model_number Индекс 3-х мерного объекта, которую необходимо удалить
   void RemoveModel(int model_number) noexcept;
+
+  /// @brief Возвращает экземпляр класса 3-х мерного объекта по индексу
+  /// @param index Индекс 3-х мерного объекта
+  /// @return Экземпляр класса 3-х мерного объекта
   ObjectModel GetModel(const unsigned int index) const noexcept;
+
+  /// @brief Возвращает размер массива 3-х мерных объектов
   std::size_t CountModel() const noexcept;
+
+  /// @brief Возвращает указтель на виджет OpenGL
   GLWidget *GetContext() const noexcept;
+
   // debug
   void PrintDate() const noexcept;
 
-  /* Model management */
+  /// @brief Метод позволяющий проводить вращение 3-х мерного объекта
+  /// @param value Значение на которое необходимо произвести вращение
+  /// @param axis Ось вокруг которой просиходит вращение
+  /// @param model_number Индекс вращаемого 3-х мерного объекта
   void Rotate(double value, char axis, int model_number);
+
+  /// @brief Метод позволяющий двигать 3-х мерный объект
+  /// @param value Значение на которое необходимо двинуть
+  /// @param axis  Ось вдоль которой происходит движение
+  /// @param model_number Инедекс перемещаемого 3-х мерного объекта
   void Move(double value, char axis, int model_number);
+
+  /// @brief Метод позволяющий масштабировать 3-х мерный объект
+  /// @param value Значение масштабирования
+  /// @param model_number Индекс масштабируемого 3-х мерного объекта
   void Scale(double value, int model_number);
 
   /* Scene management */
+
+  /// @brief Метод позволяющий сменить тип проекции
+  /// @param index Тип проекции
   void SwitchProjection(int index);
+
+  /// @brief Метод позволяющий изменить тип отображения 3-х мерной модели
+  /// @param index Тип отображения
   void SwitchWireframe(int index);
+
+  /// @brief Метод позволяющий изменить цвет сцены
+  /// @param scene_color Новый цвет сцены
   void ChangeSceneColor(QColor scene_color);
+
+  /// @brief Метод позволяющий изменить цвет вершин 3-х мерного объекта
+  /// @param vertex_color Новый цвет вершин
   void ChangeVertexColor(QVector3D vertex_color);
+
+  /// @brief Метод позволяющий изменить цвет граней 3-х мерного объекта
+  /// @param edge_color  Новый цвет граней
   void ChangeEdgeColor(QVector3D edge_color);
+
+  /// @brief Метод позволящий изменить тип отображения вершин 3-х мерного
+  /// объекта
+  /// @param index Тип отображения вершин
   void ChangeVertexOption(int index);
+
+  /// @brief Метод позволяющий изменить тип отображения граней 3-х мерного
+  /// объекта
+  /// @param index Тип отображения граней
   void ChangeEdgeOption(int index);
+
+  /// @brief Метод позволяющий изменить размер вершин 3-х мерного объекта
+  /// @param value Новое значение размера вершин
   void ChangeVertexSize(double value);
+
+  /// @brief Метод позволяющий изменить размер ребер 3-х мерного объекта
+  /// @param value Новое значение размера ребер
   void ChangeEdgeSize(double value);
 
- private:
-  std::vector<ObjectModel> models_;
-  GLWidget *context_;
+private:
+  std::vector<ObjectModel> models_; ///< Вектор 3-х мерных объектов
+  GLWidget *context_;               ///<
 };
-}  // namespace s21
+} // namespace s21
 
-#endif  // CPP4_3DVIEWER_V2_MODEL_FACADE_FACADE_H
+#endif // CPP4_3DVIEWER_V2_MODEL_FACADE_FACADE_H
