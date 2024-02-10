ac3172d0df16feeec6f743d63088adbf7118ca03
mammiemi <mammiemi@student.21-school.ru>
2024-02-10 12:16:34 +0300

    docs: struct documentation

--- src/application/model/parser/struct/coordinatestruct.h
+++ src/application/model/parser/struct/coordinatestruct.h
@@ -1,3 +1,10 @@
+/*!
+\file
+\brief Заголовочный файл с описанием структур
+
+Данный файл содержит в себе определения структур,
+которые используются для заполнения значениями координат параметров
+*/
 #ifndef CPP4_3DVIEWER_V2_MODEL_PARSER_COORDINATE_STRUCT_H
 #define CPP4_3DVIEWER_V2_MODEL_PARSER_COORDINATE_STRUCT_H
 // #include <GL/glut.h>
@@ -5,6 +12,7 @@
 
 namespace s21 {
 
+/// @brief Структура хранящая в себе значения координат точек полигона в трехмерном пространстве
 struct PointCoordinates {
   GLfloat x;
   GLfloat y;
@@ -12,12 +20,14 @@ struct PointCoordinates {
   GLfloat a{0.0};
 };
 
+/// @brief Структура хранящая в себе значения координат точек текстуры для полигона
 struct TexturesCoordinates {
   GLfloat u;
   GLfloat v;
   // GLfloat w{0.0};
 };
 
+/// @brief Структура хранящая в себе значения координат вектора нормали полигона
 struct NormalsCoordinate {
   GLfloat x;
   GLfloat y;
