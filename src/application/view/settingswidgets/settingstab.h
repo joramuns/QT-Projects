20397c40acdbfd2ebbb2c3718521d1788bfce0f4
mammiemi <mammiemi@student.21-school.ru>
2024-02-11 14:41:12 +0300

    docs: settingstab documentation

--- src/application/view/settingswidgets/settingstab.h
+++ src/application/view/settingswidgets/settingstab.h
@@ -8,13 +8,16 @@
 #include "settings.h"
 
 namespace s21 {
+/// @brief Класс определяющий инициализацию вкладок настроек
 class SettingsTab final : public QTabWidget {
   Q_OBJECT
 
- public:
+public:
+  /// @brief Конструктор по умолачанию, инициализирует поля, подключает сигналы
+  /// с обработчиками, добавляет вкладки настроек на "макет" интерфейса
   SettingsTab();
 
- public slots:
+public slots:
   void SceneColorSlot();
   void VertexColorSlot();
   void EdgeColorSlot();
@@ -25,7 +28,7 @@ class SettingsTab final : public QTabWidget {
   void VertexSizeSlot(double value);
   void EdgeSizeSlot(double value);
 
- signals:
+signals:
   void TabSceneColorSignal(QColor scene_color);
   void TabVertexColorSignal(QVector3D vertex_color);
   void TabEdgeColorSignal(QVector3D edge_color);
@@ -36,11 +39,11 @@ class SettingsTab final : public QTabWidget {
   void TabVertexSizeSignal(double value);
   void TabEdgeSizeSignal(double value);
 
- private:
+private:
   ExtraSceneSettings *scene_settings_;
   ModelSettings *vertex_settings_;
   ModelSettings *edge_settings_;
 };
-}  // namespace s21
+} // namespace s21
 
-#endif  // VIEW_SETTINGSWIDGETS_SETTINGSTAB_H_
+#endif // VIEW_SETTINGSWIDGETS_SETTINGSTAB_H_
