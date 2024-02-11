d49fd7b3c2316f721229e58810f037afbbf1ab5c
mammiemi <mammiemi@student.21-school.ru>
2024-02-11 12:01:56 +0300

    docs: tbutton documentation

--- src/application/view/transformwidgets/tbutton.h
+++ src/application/view/transformwidgets/tbutton.h
@@ -6,11 +6,15 @@
 #define BUTTON_SIZE 80
 
 namespace s21 {
+/// @brief Класс определяющий базовое поведение кнопок, отвечающих за
+/// трансформацию 3-х мерного объекта
 class TButton final : public QPushButton {
   Q_OBJECT
- public:
+public:
+  /// @brief Конструктор, инициализирующий кнопку и определяющий ее базовое поведение
+  /// @param text Отображаемый на кнопке текст 
   explicit TButton(const QString &text);
 };
-}  // namespace s21
+} // namespace s21
 
-#endif  // VIEW_TBUTTON_H_
+#endif // VIEW_TBUTTON_H_
