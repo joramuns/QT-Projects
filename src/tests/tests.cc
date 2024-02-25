4488997303e63475ff9e4d29acc582f815e2c0a0
mammiemi <mammiemi@student.21-school.ru>
2024-02-25 15:17:00 +0300

    feat(tests): add uncorrect tests case in gtests

--- src/tests/tests.cc
+++ src/tests/tests.cc
@@ -1,12 +1,39 @@
 #include "tests.h"
 
-TEST(Correct_file, test_one) {
-  s21::Parser test("/Users/mammiemi/Project/CPP4_3DViewer_v2.0-2/data-samples/test_vertex.obj");
-  std::vector<float> expect{-1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, 1.0, 0.0, -1.0, -1.0, 0.0, -1.0};
-  
+TEST(Invalid_name, test_one) {
+  EXPECT_THROW(NewParser test("kek.obj"), std::invalid_argument);
 }
 
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+TEST(Invalid_name, test_two) {
+  EXPECT_THROW(NewParser test(kPath + "cube_blender.obb"), std::invalid_argument);
+}
+
+TEST(Empty_date, empty_file) {
+  NewParser test(kPath + "empty.obj");
+  EXPECT_EQ(test.GetReadStatus(), 0);
+}
+
+TEST(Empty_date, empty_vertex) {
+  NewParser test(kPath + "empty_vertex.obj");
+  EXPECT_EQ(test.GetReadStatus() , 0);
+}
+
+TEST(Empty_date, empty_textures) {
+  NewParser test(kPath + "empty_textures.obj");
+  EXPECT_EQ(test.GetTexturesStatus() , 0);
+}
+
+TEST(Empty_date, empty_normals) {
+  NewParser test(kPath + "empty_normals.obj");
+  EXPECT_EQ(test.GetNormalsStatus(), 0);
+}
+
+TEST(Empty_date, empty_indices) {
+  NewParser test(kPath + "empty_indices.obj");
+  EXPECT_EQ(test.GetReadStatus(), 0);
+}
+
+TEST(Empty_date, out_of_range) {
+  NewParser test(kPath + "out_of_range.obj");
+  EXPECT_EQ(test.GetReadStatus(), 0);
 }
--- src/tests/tests.h
+++ src/tests/tests.h
@@ -4,6 +4,10 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-#include "../aplication/model/parser/parser.h"
+#include "../application/model/parser/new-parser.h"
+
+using namespace s21;
+
+const std::string kPath = "/home/mammiemi/Desktop/Project/CPP4_3DViewer_v2.0-2/data-samples/";
 
 #endif // CPP4_3DVIEWER_V2_TESTS_TESTS_H
