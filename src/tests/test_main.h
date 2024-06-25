e2934a4c941ccddfdf3a1912cee453e9ab5280e6
Joramun Sasin <joramuns@mi-r5.msk.21-school.ru>
2024-06-25 15:20:09 +0300

    tests: main body for tests

---
+++ src/tests/test_main.h
@@ -0,0 +1,11 @@
+#ifndef CPP4_3DVIEWER_V2_0_2_TESTS_TEST_MAIN_H_
+#define CPP4_3DVIEWER_V2_0_2_TESTS_TEST_MAIN_H_
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "../application/model/parser/parser.h"
+
+#endif  // CPP4_3DVIEWER_V2_0_2_TESTS_TEST_MAIN_H_
+
--- src/tests/tests.h
+++ src/tests/tests.h
@@ -1,13 +1,11 @@
-#ifndef CPP4_3DVIEWER_V2_TESTS_TESTS_H
-#define CPP4_3DVIEWER_V2_TESTS_TESTS_H
+#ifndef CPP4_3DVIEWER_V2_TESTS_TESTS_H_
+#define CPP4_3DVIEWER_V2_TESTS_TESTS_H_
 
-#include <gtest/gtest.h>
-#include <vector>
-
-#include "../application/model/parser/new-parser.h"
+#include "test_main.h"
 
 using namespace s21;
 
-const std::string kPath = "/home/mammiemi/Desktop/Project/CPP4_3DViewer_v2.0-2/data-samples/";
+const std::string kPath =
+    "/home/joramuns/Desktop/Project/CPP4_3DViewer_v2.0-2/data-samples/";
 
-#endif // CPP4_3DVIEWER_V2_TESTS_TESTS_H
+#endif  // CPP4_3DVIEWER_V2_TESTS_TESTS_H_
